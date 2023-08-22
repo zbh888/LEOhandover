da6075e05d443cf561b94d0f146f7a7ec6350090
Bohan Zhang <b327zhan@uwaterloo.ca>
2023-08-22 18:17:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-counter=0
 mkdir -p "res"
-for cpu in 8 16 32 64 128; do
+for cpu in 8 16 32 64 128 256 512; do
     for delay in 1 5 10 15 20 25 30; do
-        counter=$((counter + 1))
-        mkdir "res/$counter"
-        mkdir "res/$counter/graph"
-        python3 src/main.py "$counter" "$cpu" "$delay" 1> ./res/"$counter"/logs.txt 
+        DIR="${cpu}A${delay}"
+        mkdir "res/$DIR"
+        mkdir "res/$DIR/graph"
+        python3 src/main.py "$DIR" "$cpu" "$delay" 1> ./res/"$DIR"/logs.txt 
     done
 done
