45ea1861e3680aedd51727c36a4ae2e2cf5bb89f
Bohan Zhang <b327zhan@uwaterloo.ca>
2023-09-20 17:08:49 -0400

    simulation time added

--- default_run.sh
+++ default_run.sh
@@ -4,5 +4,12 @@ DIR="defaultres"
 mkdir "res/$DIR"
 mkdir "res/$DIR/graph"
 mkdir "res/$DIR/graph_data"
+
+start_time=$(date +%s)
+
 python3 src/main.py  1> ./res/"$DIR"/logs.txt
 python3 src/animation.py "res/$DIR/graph"
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+echo "Simulation takes $elapsed_time seconds"
